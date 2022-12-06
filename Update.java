533a8a5e7a7252a739ea93dd6e1b231c1cce79f6
baquy2293 <97243640+baquy2293@users.noreply.github.com>
2022-12-06 13:55:38 +0700

    Update Update.java

--- Update.java
+++ Update.java
@@ -7,34 +7,33 @@ public class Update {
     private static String USER_NAME = "root";
     private static String PASSWORD = "123456";
 
-    public static void show() {
-        try {
-
-            Connection conn = getConnection(DB_URL, USER_NAME, PASSWORD);
-
-            Statement stmt = conn.createStatement();
-
-            int rs = stmt.executeUpdate("update building set namee = 'tbq' where id  "+3  );
-
-            // close connection
-            conn.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public static Connection getConnection(String dbURL, String userName, String password) {
-        Connection conn = null;
+    public static void update() {
+        Connection connection = null;
+        Statement statement = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection(dbURL, userName, password);
-            System.out.println("connect successfully!");
-        } catch (Exception ex) {
-            System.out.println("connect failure!");
-            ex.printStackTrace();
+            connection = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            statement = connection.createStatement();
+            // dem so dong cua cot
+            int count = 0;
+            ResultSet rs = statement.executeQuery("select * from building");
+            while (rs.next()) {
+                count++;
+            }
+            statement.executeUpdate("update building \n" +
+                    "set namee = 'tbq'\n" +
+                    "where id<" + count + 1);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                statement.close();
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        return conn;
     }
-
-
 }
