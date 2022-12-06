94124095a64e23499cbf8a5a9908f3a731141a1c
baquy2293 <97243640+baquy2293@users.noreply.github.com>
2022-12-06 13:54:46 +0700

    Update Create.java

--- Create.java
+++ Create.java
@@ -10,37 +10,36 @@ public class Create {
     private static String USER_NAME = "root";
     private static String PASSWORD = "123456";
 
-    public static void show() {
-        try {
-
-            Connection conn = getConnection(DB_URL, USER_NAME, PASSWORD);
-
-            Statement statement = conn.createStatement();
-
-//          statement.executeUpdate(" alter table building add column color varchar(15)");
-ResultSet resultSet = statement.executeQuery("select * from building");
-          while (resultSet.next())
-          {
-              statement.executeUpdate;
-          }
-            // close connection
-            conn.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public static Connection getConnection(String dbURL, String userName, String password) {
-        Connection conn = null;
+    public static void create() {
+        Connection connection = null;
+        Statement statement = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection(dbURL, userName, password);
-            System.out.println("connect successfully!");
-        } catch (Exception ex) {
-            System.out.println("connect failure!");
-            ex.printStackTrace();
+            connection = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            statement = connection.createStatement();
+            statement.executeUpdate(" alter table building add column color varchar(15)");
+            // dem so dong cua cot
+            int count=0;
+            ResultSet rs =  statement.executeQuery("select * from building");
+            while (rs.next())
+            {
+                count++;
+            }
+            statement.executeUpdate("update building \n" +
+                    "set color = 'red'\n" +
+                    "where id<"+count+1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                statement.close();
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        return conn;
+
     }
 
 }
